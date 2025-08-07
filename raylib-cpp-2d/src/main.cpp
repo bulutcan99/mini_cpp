d100463451ce5c5f6611831425d6055a9a6bdafb
bulutcan99 <gocerbulutcan@gmail.com>
2025-08-07 21:26:46 +0300

    Initialize Raylib C++ project with submodule and update main entry point

--- raylib-cpp-2d/src/main.cpp
+++ raylib-cpp-2d/src/main.cpp
@@ -1,20 +1,13 @@
 #include "raylib.h"
 
 int main() {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-
-    InitWindow(screenWidth, screenHeight, "Raylib C++ Starter");
-
+    InitWindow(800, 600, "CLion + C++ + Raylib");
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-
-        DrawText("Hello, Raylib + C++!", 190, 200, 20, DARKGRAY);
-        DrawRectangle(100, 100, 50, 50, RED);
-
+        DrawText("Hello from C++ and Raylib!", 180, 200, 20, DARKGRAY);
         EndDrawing();
     }
 
